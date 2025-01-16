bd0daee8403fc368c3929f29e0b27ef6b7df7dc9
adrianRodriguez77 <adrian.rodriguez.vegas.7e6@itb.cat>
2025-01-16 16:47:01 +0100

    Nueva fuente, header con imagen y nav fijo

--- protactics/src/components/Header.vue
+++ protactics/src/components/Header.vue
@@ -7,13 +7,12 @@
           <a href="#">PERFIL</a>
           <a href="#">CONTACTO</a>
         </nav>
-        <img src="../assets/img/bannerHeader.png" alt="">
+        <img class="banner" src="../assets/img/bannerHeader.png" alt="">
       </header>
       
   </template>
   
   <script setup>
-  // No hay lógica específica para el header por ahora.
   </script>
   
   <style scoped>
