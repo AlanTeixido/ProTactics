bf53c900addee4fdac38a2514d1cbbb78919f8a5
AlanTeixido <alan.teixido.7e7@itb.cat>
2025-01-31 10:11:57 +0100

    refactor: Clean up unused styles and enhance text presentation in components

--- protactics/src/components/MainSection.vue
+++ protactics/src/components/MainSection.vue
@@ -16,31 +16,5 @@
 </template>
 
 <script setup>
-// No hay lógica específica para esta sección por ahora.
 </script>
 
-<style scoped>
-.text h1 {
-  font-family: 'Bebas Neue', sans-serif;
-  font-size: 3rem;
-  margin-bottom: 1rem;
-  color: white;
-}
-
-.main-paragraph,
-.secondary-paragraph {
-  font-size: 1.2rem;
-  color: white;
-  font-weight: 300;
-  line-height: 1.6;
-  margin-bottom: 1.5rem;
-  transition: color 0.3s ease, transform 0.3s ease;
-}
-
-.main-paragraph:hover,
-.secondary-paragraph:hover {
-  color: rgb(0, 195, 255); /* Cambia el color a azul claro */
-  transform: translateY(-5px); /* Efecto de desplazamiento para un toque sutil */
-}
-
-</style>
